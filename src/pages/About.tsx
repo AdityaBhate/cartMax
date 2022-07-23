71c8ade83c3f2ce632b539daf4ef1307452d8e47
Aditya Bhate <adityabhate305@gmail.com>
2022-07-23 11:13:06 +0530

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,16 @@
+export function About() {
+	return (
+		<>
+			<h1>About</h1>
+			<p className='fs-1'>
+				CartMax is Advanced shopping cart.
+				<br />
+				Add Different types of items
+				<br />
+				of different quantity and price to the cart.
+				<hr />
+				Made with React and Typescript
+			</p>
+		</>
+	);
+}
